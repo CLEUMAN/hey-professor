8c610ab7a87c4e4848d8d30c867f2a29aeee5c77
CLEUMAN <cleuman11@hotmail.com>
2024-03-04 12:45:26 -0300

    should check if ends with question mark ?

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -9,7 +9,7 @@ it('should be able to create a new question bigger than 255 characters', functio
 
     //  ACT:: AGIR
     $request = \Pest\Laravel\post(route('question.store'), [
-        'question' => str_repeat('*', 260),
+        'question' => str_repeat('*', 260).'?',
 
     ]);
 
@@ -17,12 +17,31 @@ it('should be able to create a new question bigger than 255 characters', functio
     // ASSERT:: VERIFICAR
     $request->assertRedirect(route('dashboard'));
     \Pest\Laravel\assertDatabaseCount('questions', 1);
-    \Pest\Laravel\assertDatabaseHas('questions', ['question' => str_repeat('*', 260)]);
+    \Pest\Laravel\assertDatabaseHas('questions', ['question' => str_repeat('*', 260).'?']);
 
 
 });
 
 it('should check if ends with question mark ?', function(){
+    // ARRANGE:: PREPARAR
+    $user = \App\Models\User::factory()->create();
+    \Pest\Laravel\actingAs($user);
+
+    //  ACT:: AGIR
+    $request = \Pest\Laravel\post(route('question.store'), [
+        'question' => str_repeat('*', 10),
+
+    ]);
+
+
+    // ASSERT:: VERIFICAR
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question ? It is missing the question mark in the end'
+    ]);
+    \Pest\Laravel\assertDatabaseCount('questions', 0);
+});
+
+it('should have at least 10 characters', function(){
     // ARRANGE:: PREPARAR
     $user = \App\Models\User::factory()->create();
     \Pest\Laravel\actingAs($user);
@@ -37,8 +56,5 @@ it('should check if ends with question mark ?', function(){
     // ASSERT:: VERIFICAR
     $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
     \Pest\Laravel\assertDatabaseCount('questions', 0);
-});
 
-it('shouldbhave at least 10 characters', function(){
-    expect(true)->toBeTrue();
 });

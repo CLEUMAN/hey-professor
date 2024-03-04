ce70aaf255eb7755e61230c490d63747fc33dec3
CLEUMAN <cleuman11@hotmail.com>
2024-03-04 12:12:05 -0300

    should check if ends with question mark ?

--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -23,7 +23,20 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should check if ends with question mark ?', function(){
-    expect(true)->toBeTrue();
+    // ARRANGE:: PREPARAR
+    $user = \App\Models\User::factory()->create();
+    \Pest\Laravel\actingAs($user);
+
+    //  ACT:: AGIR
+    $request = \Pest\Laravel\post(route('question.store'), [
+        'question' => str_repeat('*', 8)."?",
+
+    ]);
+
+
+    // ASSERT:: VERIFICAR
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+    \Pest\Laravel\assertDatabaseCount('questions', 0);
 });
 
 it('shouldbhave at least 10 characters', function(){

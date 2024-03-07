df3fba31016294804dcd41ee326c4fcf637b7c3f
CLEUMAN <cleuman11@hotmail.com>
2024-03-06 23:38:28 -0300

    being able to see the list of question in the dashboard

---
+++ resources/views/components/question/card.blade.php
@@ -0,0 +1,5 @@
+@props([
+'question'
+])
+
+<div class="dark:text-gray-400 my-4 p-2 rounded bg-white shadow shadow-blue-500/50">{{$question->question}}</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,5 +16,13 @@
 
             </x-question.form>
 
+        <hr class="border-gray-700 border-dashed my-4">
+
+        <div class="dark:text-gray-400 uppercase font-bold">## List of questions</div>
+
+        @foreach($questions as $question)
+            <x-question.card :question="$question"/>
+        @endforeach
+
     </x-layout.container>>
 </x-app-layout>
